2342193972d3c9ffa55c697642dccd04bd52ca68
fayadh <fayadh.almosawi@gmail.com>
2015-05-22 01:41:13 -0400

    added routing functionality to the command line

--- famous/BOT.g/app.rb
+++ famous/BOT.g/app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader' if development? 
 
 get '/' do
 	erb :index 
+end
+
+# get '/:open_world' do 
+# 	erb :open_world
+# end
+
+post '/' do
+	puts params
+	if params[:command] == 'open world'
+		erb :open_world
+	else 
+		erb :other
+	end
 end
