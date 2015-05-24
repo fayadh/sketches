f236090313d71c2d136fe7dc52e18418b598a1a2
fayadh <fayadh.almosawi@gmail.com>
2015-05-24 18:54:23 -0400

    added DB and fixed border function

--- famous/BOT.g/public/js/main_game_with_scores.js
+++ famous/BOT.g/public/js/main_game_with_scores.js
@@ -26,7 +26,7 @@ function randomIntFromInterval(min,max)
 }
 
 var rows = 19;
-var columns = 20;
+var columns = 19;
 
 //BACKGROUND
 	var backgroundSurface = new Surface({ 
@@ -314,11 +314,448 @@ createRandomBlocks()
 		}
 	}
 
-//scores from eating
+
+///
+
+	function DrawLine(line) {
+		length = line.length
+		for(var j = 0; j < length; j++) {
+			point = line[j];
+			beadCollector[ point[0] ][ point[1] ].setProperties({
+				backgroundColor: 'white',
+				boxShadow: '10px 5px 5px green'
+			});
+		};
+	};
+// --OUTPUT -- //
+	function Draw(thing) {
+		for(i = 0; i < thing.length; i++) {
+			// console.log(thing[i])
+			DrawLine(thing[i]);
+		};
+	};
+
+//IMAGE DB
+
+//STRAIGHT LINE FUNCTION
+	function StraightLine(mode, startingPoint, length) {
+		array = [];
+		for(var i = 0; i < length; i++) {
+			if(mode === "hor") {
+				array.push([ startingPoint[0], startingPoint[1] + i]);
+			}
+			else if(mode === "ver") {
+				array.push([ startingPoint[0] + i, startingPoint[1] ]);
+			}
+		};
+		return array;
+	};
+
+	
+
+//DRAW
+function Draw_Object(input) {
+	console.log(input)
+	for(var i = 0; i < input.length; i++) {
+		array = [ StraightLine(input[i].mode, input[i].start, input[i].length) ];
+		console.log(array)
+		Draw(array); //Draw expects an array
+	};
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Object DB 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//BORDERS
+		var Border1 = [
+			{mode: "ver", start: [0, rows - 1], length: rows},
+			{mode: "ver", start: [0, 0], length: rows},
+
+			{mode: "hor", start: [columns - 1, 0], length: columns},
+			{mode: "hor", start: [0, 0], length: columns},
+		]
+
+		
+//LETTERS
+	//CAPITAL LETTERS DB
+		var A = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 4], length: 10},
+			{mode: "ver", start: [0, 5], length: 10},
+		]
+
+		var B = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+
+			{mode: "ver", start: [0, 1], length: 10},
+			{mode: "ver", start: [0, 4], length: 10},
+			{mode: "ver", start: [0, 5], length: 10},
+		]
+
+		var C = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+		]
+
+		var D = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
 
 
-	createRandomFoods()
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
 
+			{mode: "ver", start: [0, 1], length: 10},
+			{mode: "ver", start: [0, 4], length: 10},
+			{mode: "ver", start: [0, 5], length: 10},
+		]
+
+		var E = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+		]
+
+		var F = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+		]
+
+		var G = [
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 3], length: 3},
+			{mode: "hor", start: [5, 3], length: 3},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [4, 4], length: 4},
+			{mode: "ver", start: [4, 5], length: 4},
+		]
+
+		var H = [
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 4], length: 10},
+			{mode: "ver", start: [0, 5], length: 10},
+		]
+
+		var I = [
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "ver", start: [0, 2], length: 10},
+			{mode: "ver", start: [0, 3], length: 10},
+		]
+
+		var J = [
+			{mode: "hor", start: [0, 1], length: 5},
+			{mode: "hor", start: [1, 1], length: 5},
+
+			{mode: "hor", start: [6, 0], length: 2},
+			{mode: "hor", start: [7, 0], length: 2},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 1], length: 4},
+
+			{mode: "ver", start: [0, 4], length: 10},
+			{mode: "ver", start: [0, 5], length: 9},
+		]
+
+		var K = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "hor", start: [0, 4], length: 2},
+			{mode: "hor", start: [1, 4], length: 2},
+			{mode: "hor", start: [2, 4], length: 2},
+
+			{mode: "hor", start: [3, 3], length: 1},
+			{mode: "hor", start: [4, 2], length: 1},
+			{mode: "hor", start: [5, 2], length: 1},
+			{mode: "hor", start: [6, 3], length: 1},
+			{mode: "hor", start: [7, 4], length: 2},
+			{mode: "hor", start: [8, 4], length: 2},
+			{mode: "hor", start: [9, 4], length: 2},
+		]
+
+		var L = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+		]
+
+		var M = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 5], length: 10},
+			{mode: "ver", start: [0, 6], length: 10},
+
+			{mode: "hor", start: [4, 2], length: 1},
+			{mode: "hor", start: [5, 3], length: 1},
+			{mode: "hor", start: [4, 4], length: 1},
+		]
+
+		var N = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 5], length: 10},
+			{mode: "ver", start: [0, 6], length: 10},
+
+			{mode: "hor", start: [2, 2], length: 1},
+
+			{mode: "hor", start: [3, 2], length: 1},
+			{mode: "hor", start: [3, 3], length: 1},
+
+			{mode: "hor", start: [4, 2], length: 1},
+			{mode: "hor", start: [4, 3], length: 1},
+			{mode: "hor", start: [4, 4], length: 1},
+
+			{mode: "hor", start: [5, 3], length: 1},
+			{mode: "hor", start: [5, 4], length: 1},
+
+			{mode: "hor", start: [6, 4], length: 1},
+		]
+
+		var O = [
+			{mode: "ver", start: [1, 0], length: 8},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 5], length: 10},
+			{mode: "ver", start: [1, 6], length: 8},
+
+			{mode: "hor", start: [0, 1], length: 4},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 1], length: 4},
+		]
+
+		var P = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 4], length: 5},
+			{mode: "ver", start: [0, 5], length: 5},
+
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 6},
+		]
+
+		var Q = []
+
+		var R = [
+			{mode: "ver", start: [0, 0], length: 10},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 4], length: 5},
+			{mode: "ver", start: [0, 5], length: 4},
+
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [4, 0], length: 6},
+			{mode: "hor", start: [5, 0], length: 5},
+
+			{mode: "hor", start: [6, 3], length: 1},
+			{mode: "hor", start: [7, 4], length: 1},
+			{mode: "hor", start: [8, 4], length: 2},
+			{mode: "hor", start: [9, 4], length: 2},
+		]
+
+		var S = []
+		var T = [ 
+			{mode: "ver", start: [0, 2], length: 10},
+			{mode: "ver", start: [0, 3], length: 10},
+
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+		]
+		var U = [
+			{mode: "ver", start: [0, 0], length: 9},
+			{mode: "ver", start: [0, 1], length: 10},
+
+			{mode: "ver", start: [0, 5], length: 10},
+			{mode: "ver", start: [0, 6], length: 9},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 1], length: 4},
+		]
+
+		var V = [
+			{mode: "hor", start: [1, 0], length: 2},
+			{mode: "hor", start: [2, 0], length: 2},
+			{mode: "hor", start: [3, 0], length: 2},
+			{mode: "hor", start: [4, 0], length: 3},
+			{mode: "hor", start: [5, 1], length: 2},
+			{mode: "hor", start: [6, 1], length: 2},
+
+			{mode: "hor", start: [7, 2], length: 3},
+			{mode: "hor", start: [8, 2], length: 3},
+			{mode: "hor", start: [9, 3], length: 1},
+
+			{mode: "hor", start: [6, 4], length: 2},
+			{mode: "hor", start: [5, 4], length: 2},
+			{mode: "hor", start: [4, 4], length: 3},
+			{mode: "hor", start: [3, 5], length: 2},
+			{mode: "hor", start: [2, 5], length: 2},
+			{mode: "hor", start: [1, 5], length: 2},
+		]
+		var W = [
+			{mode: "hor", start: [0, 0], length: 1},	
+			{mode: "hor", start: [1, 0], length: 2},
+			{mode: "hor", start: [2, 0], length: 2},
+			{mode: "hor", start: [3, 0], length: 2},
+			{mode: "hor", start: [4, 0], length: 3},
+			{mode: "hor", start: [5, 1], length: 2},
+			{mode: "hor", start: [6, 1], length: 2},
+
+			{mode: "hor", start: [7, 2], length: 3},
+			{mode: "hor", start: [8, 2], length: 3},
+			{mode: "hor", start: [9, 3], length: 1},
+
+			{mode: "hor", start: [6, 4], length: 2},
+			{mode: "hor", start: [5, 4], length: 5},
+			{mode: "hor", start: [4, 4], length: 5},
+			{mode: "hor", start: [3, 5], length: 3},
+			{mode: "hor", start: [2, 5], length: 3},
+			{mode: "hor", start: [1, 5], length: 3},
+
+			{mode: "hor", start: [0, 6], length: 1},
+
+			{mode: "hor", start: [6, 7], length: 2},	
+			{mode: "hor", start: [7, 8], length: 3},
+			{mode: "hor", start: [8, 8], length: 3},	
+			{mode: "hor", start: [9, 9], length: 1},
+
+			{mode: "hor", start: [0, 12], length: 1},	
+			{mode: "hor", start: [1, 11], length: 2},	
+			{mode: "hor", start: [2, 11], length: 2},	
+			{mode: "hor", start: [3, 11], length: 2},	
+			{mode: "hor", start: [4, 10], length: 3},	
+			{mode: "hor", start: [5, 10], length: 2},	
+			{mode: "hor", start: [6, 10], length: 2}	
+		]
+
+		var X = [
+			{mode: "hor", start: [0, 0], length: 1},
+			{mode: "hor", start: [0, 5], length: 1},
+
+			{mode: "hor", start: [1, 0], length: 1},
+			{mode: "hor", start: [1, 5], length: 1},
+
+			{mode: "hor", start: [2, 0], length: 2},
+			{mode: "hor", start: [2, 4], length: 2},
+
+			{mode: "hor", start: [3, 1], length: 4},
+
+			{mode: "hor", start: [4, 2], length: 2},
+			{mode: "hor", start: [5, 2], length: 2},
+
+			{mode: "hor", start: [6, 1], length: 1},
+			{mode: "hor", start: [6, 4], length: 1},
+
+			{mode: "ver", start: [7, 0], length: 3},
+			{mode: "ver", start: [7, 5], length: 3},
+		]
+
+		var Y = []	
+
+
+		var Z = [ 
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [0, 0], length: 6},
+			{mode: "hor", start: [1, 0], length: 6},
+
+			{mode: "hor", start: [2, 4], length: 2},
+
+			{mode: "hor", start: [3, 4], length: 1},
+			{mode: "hor", start: [4, 3], length: 1},
+			{mode: "hor", start: [5, 2], length: 1},
+			{mode: "hor", start: [6, 1], length: 1},
+
+			{mode: "hor", start: [7, 0], length: 2},
+
+			{mode: "hor", start: [8, 0], length: 6},
+			{mode: "hor", start: [9, 0], length: 6},
+		]	
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Objects To Be Drawn 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+Draw_Object(Border1);
+createRandomFoods()
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// AUTO BOT
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	
 //Time the animation
 	//first way
 	var imageAnimationSwitch = 0;
